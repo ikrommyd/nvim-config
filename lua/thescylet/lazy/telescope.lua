2dd191b7558fbcdbc1821fd640e3a5c52a6bbb07
Iason Krommydas <iason.krom@gmail.com>
2025-02-17 02:45:45 -0600

    add search in folders

--- lua/thescylet/lazy/telescope.lua
+++ lua/thescylet/lazy/telescope.lua
@@ -4,11 +4,15 @@ return {
     branch = "0.1.x",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        "nvim-telescope/telescope-file-browser.nvim",
     },
 
     config = function()
-        require('telescope').setup({})
+        local telescope = require('telescope')
+        telescope.setup({})
+        telescope.load_extension('live_grep_args')
 
         local builtin = require('telescope.builtin')
         -- Global variable to store the last search term
@@ -43,6 +47,40 @@ return {
                 print("No previous search term stored.")
             end
         end)
+
+        vim.keymap.set('n', '<leader>fs', telescope.extensions.live_grep_args.live_grep_args, {})
+
+        local ts_select_dir_for_grep = function(prompt_bufnr)
+          local action_state = require("telescope.actions.state")
+          local fb = require("telescope").extensions.file_browser
+          local live_grep = require("telescope.builtin").live_grep
+          local current_line = action_state.get_current_line()
+
+          fb.file_browser({
+            files = false,
+            depth = false,
+            attach_mappings = function(prompt_bufnr)
+              require("telescope.actions").select_default:replace(function()
+                local entry_path = action_state.get_selected_entry().Path
+                local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+                local relative = dir:make_relative(vim.fn.getcwd())
+                local absolute = dir:absolute()
+
+                live_grep({
+                  results_title = relative .. "/",
+                  cwd = absolute,
+                  default_text = current_line,
+                })
+              end)
+
+              return true
+            end,
+          })
+        end
+
+        vim.keymap.set('n', '<C-f>', ts_select_dir_for_grep, {})
+        vim.keymap.set('i', '<C-f>', ts_select_dir_for_grep, {})
+
     end
 }
 
