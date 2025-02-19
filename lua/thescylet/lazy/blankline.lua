03756f32292ce063db89e158290d4308efaaa909
Iason Krommydas <iason.krom@gmail.com>
2025-02-18 19:47:38 -0600

    do not show scope

--- lua/thescylet/lazy/blankline.lua
+++ lua/thescylet/lazy/blankline.lua
@@ -1,10 +1,12 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
     opts = {},
     config = function()
-        require("ibl").setup()
+        require("ibl").setup({
+            scope = {
+                enabled = false,
+            }
+        })
     end
 }
