900f6026b1f40ec700c33c77cc13d78083a677b1
Iason Krommydas <iason.krom@gmail.com>
2025-04-10 15:35:55 -0500

    add tokyonight

--- lua/thescylet/lazy/lualine.lua
+++ lua/thescylet/lazy/lualine.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require('lualine').setup {
             options = {
-                theme = "onedark",
+                theme = "tokyonight",
             },
             sections = {
                 lualine_c = {
